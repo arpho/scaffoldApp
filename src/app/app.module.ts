80ce76bccdeb184068b8cb805ebde0c7bdd45652
arpho <damicogiuseppe77@gmail.com>
2023-07-05 19:38:47 +0200

    ion-header is not a known element
    resetpassword page was not declared in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { BarComponent } from './components/bar/bar.component';
 import { FilterPopupPage } from './modules/item/pages/filter-popup/filter-popup.page';
 import { CookiesLawComponent } from './components/cookies-law/cookies-law.component';
 import { MenuPage } from './pages/menu/menu.page';
+import { ResetPasswordPage } from './modules/user/pages/reset-password/reset-password.page';
 
 
 
@@ -35,6 +36,7 @@ import { MenuPage } from './pages/menu/menu.page';
         BarComponent,
         FilterPopupPage,
         CookiesLawComponent,
+        ResetPasswordPage,
         MenuPage,
     ],
     imports: [

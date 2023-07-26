64a66a3a20d0c4c5c964cd97b240f440f028b3a5
arpho <damicogiuseppe77@gmail.com>
2023-07-26 09:30:14 +0200

    duplicated sleek-login-component
    just to have a starting point

--- src/app/modules/user/components/scout-login/scout-login.component.ts
+++ src/app/modules/user/components/scout-login/scout-login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { UntypedFormGroup, UntypedFormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoadingController, AlertController } from '@ionic/angular';
+import { getAuth, setPersistence, browserLocalPersistence } from 'firebase/auth';
+import { configs, registrationStrategy } from 'src/app/configs/configs';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-scout-login',
@@ -7,8 +13,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScoutLoginComponent implements OnInit {
 
-  constructor() { }
+  public loginForm: UntypedFormGroup;
+  public loading: HTMLIonLoadingElement;
+  buttonText = ""
+  ngOnInit() {
+    if (configs.registrationStrategy == registrationStrategy.regular) {
+      this.buttonText = "Registra Utente"
+    }
+    if (configs.registrationStrategy == registrationStrategy.twoSteps) {
+      this.buttonText = "Completa la registrazione "
+    }
 
-  ngOnInit() {}
+
+  }
+
+  constructor(
+    public loadingCtrl: LoadingController,
+    public alertCtrl: AlertController,
+    private authService: AuthService,
+    private router: Router,
+    private formBuilder: UntypedFormBuilder
+  ) {
+    this.loginForm = this.formBuilder.group({
+      email: ["", Validators.compose([Validators.required, Validators.email])],
+      password: [
+        "",
+        Validators.compose([Validators.required, Validators.minLength(6)])
+      ]
+    });
+  }
+  onSubmit() { }
+
+  register() {
+    if (configs.registrationStrategy == registrationStrategy.regular) {
+      this.router.navigateByUrl("users/signup")
+    }
+    if (configs.registrationStrategy == registrationStrategy.twoSteps) {
+      this.router.navigateByUrl("users/complete-registration")
+    }
+  }
+  async loginUser(loginForm: UntypedFormGroup): Promise<void> {
+
+    if (!loginForm.valid) {
+      console.log("Form is not valid yet, current value:", loginForm.value);
+    } else {
+      const email = loginForm.value.email;
+      const password = loginForm.value.password;
+      this.authService.loginUser(email, password).then(
+        () => {
+          const auth = getAuth()
+          setPersistence(auth, browserLocalPersistence)
+          this.loading.dismiss().then(() => {
+            this.router.navigateByUrl("home");
+          });
+        },
+        error => {
+          this.loading.dismiss().then(async () => {
+            const alert = await this.alertCtrl.create({
+              message: error.message,
+              buttons: [{ text: "Ok", role: "cancel" }]
+            });
+            await alert.present();
+          });
+        }
+      );
+      this.loading = await this.loadingCtrl.create();
+      await this.loading.present();
+    }
+  }
 
 }

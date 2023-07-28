eb936ac47638ca6b2a5f0ef508bafdc937f2bf2b
arpho <damicogiuseppe77@gmail.com>
2023-07-28 12:25:52 +0200

    ready to send request to kempelen
    with the help of chatGPT we prepared the request

--- src/app/services/kempelen-service.service.ts
+++ src/app/services/kempelen-service.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
@@ -10,11 +10,21 @@ export class KempelenService {
   constructor(private http: HttpClient) { }
 
    fetchAsset(ownerKey,assetId){
-    const options = {
-      responseType:'json'
-    }
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer myAuthToken',
+      'Content-Type': 'application/json'
+    });
+    const params = new HttpParams()
+    params.set("owner",ownerKey).set('id',assetId)
+     const options = {
+      headers: headers,
+      params: params,
+      observe: 'body' as const,
+      responseType: 'json' as const,
+      withCredentials: true
+    };
 
     const url ="https://dev.thinkingadditive.com/v0/providers/export/requests"
-return this.http.get
+return this.http.get(url,options)
   }
 }

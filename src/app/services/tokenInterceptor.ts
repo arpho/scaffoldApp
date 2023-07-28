446fe2ce45808c1173e8cb54d0452d006b8534a6
arpho <damicogiuseppe77@gmail.com>
2023-07-28 11:11:14 +0200

    implemented tokenInterceptor.ts
    it is a class tha implements HttpInterceptors to add the Bearer token to the http requests

---
+++ src/app/services/tokenInterceptor.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { AuthService } from '../modules/user/services/auth.service';
+import { Observable } from 'rxjs';
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+  constructor(public auth: AuthService) {}
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    let token =""
+    const tokenHandler = (Token)=>{
+        token = Token
+        request = request.clone({
+            setHeaders: {
+              Authorization: `Bearer ${token}`
+            }
+          });
+          next.handle(request);
+    }
+    this.auth.getToken(tokenHandler)
+    
+   
+    return 
+  }
+}

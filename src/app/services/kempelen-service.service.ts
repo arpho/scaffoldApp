628493d15f749eb8fe441cfbae90581886bb78c8
arpho <damicogiuseppe77@gmail.com>
2023-07-28 10:53:29 +0200

    implementing kempelenService.getAsset
    implementing the method to fetch an asset from paperspace

--- src/app/services/kempelen-service.service.ts
+++ src/app/services/kempelen-service.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class KempelenService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+   fetchAsset(ownerKey,assetId){
+    const options = {
+      responseType:'json'
+    }
+
+    const url ="https://dev.thinkingadditive.com/v0/providers/export/requests"
+return this.http.get
+  }
 }

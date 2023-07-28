3d5953782a82d3a61dc3fe5315f27fad256e820d
arpho <damicogiuseppe77@gmail.com>
2023-07-28 09:36:39 +0200

    prepare form in home page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { credentials } from 'src/app/configs/credentials';
+import { TextboxQuestion } from 'src/app/modules/dynamic-form/models/question-textbox';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  submitText=""
+  formFields=[
+    new TextboxQuestion({
+      key:"documen_id",
+      label:"document id"
+    })
+  ]
 
   constructor() { }
+  filter(ev){
+    console.log("typing",ev)
+  }
 
-  ngOnInit() {
+  submit(ev){
+    console.log("submitted",ev)
+  }
+
+  async ngOnInit() {
+    const isKarma = document.getElementsByTagName("title")[0].innerHTML === 'Karma';
+    if(!isKarma) // we check it is not a test
+    {
+      const app = initializeApp(credentials.firebase)
+      const auth = getAuth()
+      const onAuthStateChangedHandler =async  (user)=>{
+        console.log("got user",user)
+        const token = await user.getIdTokenResult()
+        console.log("token", token.token)
+        this.submitText = `get document for user ${user.uid}`
+      }
+      onAuthStateChanged(auth,onAuthStateChangedHandler)
+    }
+
   }
 
 }

541bb43441b17b83e9c9d88c48db354aa85ab9c8
arpho <damicogiuseppe77@gmail.com>
2023-08-03 09:58:21 +0200

    refactored home page to switch background

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -6,6 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  blue= true
+  text="sfondo blu"
+
+  changeEvent(ev){
+    this.blue= !this.blue
+    console.log("evento",ev,this.blue)
+    if(this.blue){
+      this.text="sfondo blu"
+    }
+    else{
+      this.text="sfondo bianco"
+    }
+  }
 
   constructor() { }
 

6a29bc75abae4ebdfd4397aa83ff71cbd1f34801
arpho <damicogiuseppe77@gmail.com>
2023-08-03 18:00:35 +0200

    reinitialized firebase project

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,63 +1,21 @@
-import * as functions from "firebase-functions";
-import * as express from "express";
-import * as admin from "firebase-admin";
-import {addEntry,
-  deleteEntry,
-  getAllEntries,
-  updateEntry,
-} from "./entryController";
-import {addExtraction} from "./extractionController";
-import {
-  addUserProfile,
-  getUserProfile,
-  updateUserProfile,
-} from "./insertUserProfile";
-if (admin.apps.length === 0) {
-  admin.initializeApp();
-}
-const app = express();
-app.get("/", (req, res) => res.status(200).send("Hey there!"));
-app.post("/entries", addEntry);
-app.post("/userProfile", addUserProfile);
-exports.app = functions.https.onRequest(app);
-app.get("/entries", getAllEntries);
-app.patch("/entries/:entryId", updateEntry);
-app.delete("/entries/:entryId", deleteEntry);
-app.post("/extraction", addExtraction);
-exports.adminAddUserProfile = functions.https.onCall((data)=>{
-  addUserProfile(data);
-});
-
-exports.addCustomClaim = functions.https.onCall((data) => {
-  return admin.auth().getUserByEmail(data.email).then((user) => {
-    return admin.auth().setCustomUserClaims(user.uid, data.claims).
-        then(() => {
-          return {
-            message: ` Success!claims ${Object.keys(data.claim)} 
-        have been set on ${data.email}`,
-          };
-        }).catch((err) => {
-          return {error: err};
+import {onRequest} from "firebase-functions/v1/https";
+import fetch from "node-fetch";
+exports.getAsset = onRequest(async (req: any, res: any) => {
+  // Grab the text parameter.
+  const owner = req.query.owner;
+  const assetId = req.query.assetId;
+  const url = "https://dev.thinkingadditive.com/v0/providers/export/requests"; // Sostituisci con l'URL del tuo server backend esterno
+  const urlFactory = (url:string, owner:string, assetId:string) => {
+    return `${url}?owner=${owner}&id=${assetId}`;
+  };
+  try {
+    const response = await fetch(urlFactory(url, owner, assetId));
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("Errore durante la richiesta:", error);
+    res.status(500)
+        .send({text: "Errore durante la richiesta al server backend.", error,
         });
-  });
+  }
 });
-
-exports.deleteAuthUser = functions.https.onCall((data)=>{
-  return admin.auth().getUserByEmail(data.email).then((user)=>{
-    return admin.auth().deleteUser(user.uid).
-        then(()=> {
-          return {message: `Success! user ${data.email} has been deleted!`};
-        }).
-        catch((err)=>{
-          return {error: err};
-        });
-  });
-});
-
-exports.getUserProfile =functions.https.onCall((data)=>{
-  return getUserProfile(data);
-});
-exports.updateUserProfile= functions.https.onCall((data)=>{
-  return updateUserProfile(data);
-}
-);

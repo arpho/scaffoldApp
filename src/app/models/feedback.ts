ec3ad5cf7f7450842f9a0436c3cffdc9065ab73a
arpho <damicogiuseppe77@gmail.com>
2023-08-07 16:50:41 +0200

    created feedback model

---
+++ src/app/models/feedback.ts
@@ -0,0 +1,23 @@
+export class Feedback {
+
+    name: string;
+
+    gender: string;
+
+    rating: string;
+
+    comment: string;
+
+    constructor() {
+
+        this.name = '';
+
+        this.gender = '';
+
+        this.rating = '';
+
+        this.comment = '';
+
+    }
+
+}

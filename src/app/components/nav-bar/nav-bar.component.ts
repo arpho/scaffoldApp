4bda95f37b1294d76243d8a82cc54344c87d9944
arpho <damicogiuseppe77@gmail.com>
2023-08-07 16:50:10 +0200

    refactored nav-bar.component

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,14 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { BsDropdownConfig } from 'ngx-bootstrap/dropdown';
+
+import { TranslocoService } from '@ngneat/transloco';
 
 @Component({
+
   selector: 'app-nav-bar',
+
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss'],
+
+  styleUrls: ['./nav-bar.component.css'],
+
+  providers: [{ provide: BsDropdownConfig, useValue: { isAnimated: true, autoClose: true } }]
+
 })
-export class NavBarComponent implements OnInit {
 
-  constructor() { }
+export class NavBarComponent {
 
-  ngOnInit() {}
+  siteLanguage = 'English';
+
+  languageList = [
+
+    { code: 'en', label: 'English' },
+
+    { code: 'es', label: 'Español' },
+
+    { code: 'pt', label: 'Português' }
+
+  ];
+
+  constructor(private service: TranslocoService) { }
+
+  changeSiteLanguage(language: string): void {
+
+    this.service.setActiveLang(language);
+
+    this.siteLanguage = this.languageList.find(f => f.code === language).label;
+
+  }
 
 }

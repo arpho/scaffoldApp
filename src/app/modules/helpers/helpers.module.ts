aa65d040d1b262d9c9daac03517134356e4742eb
arpho <damicogiuseppe77@gmail.com>
2023-08-09 22:01:18 +0200

    removed translate pipe from helpers module

--- src/app/modules/helpers/helpers.module.ts
+++ src/app/modules/helpers/helpers.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyToastService } from './services/toaster/my-toast-service.service';
-import { TranslatePipe } from './pipes/translate.pipe';
 
 
 
 @NgModule({
   declarations: [
-    TranslatePipe
   ],
   imports: [
     CommonModule

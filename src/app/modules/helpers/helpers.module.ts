63dc1e9debc25d2152603f81db142fd20247cda5
arpho <damicogiuseppe77@gmail.com>
2023-08-09 21:36:05 +0200

    declared translate pipe

--- src/app/modules/helpers/helpers.module.ts
+++ src/app/modules/helpers/helpers.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyToastService } from './services/toaster/my-toast-service.service';
+import { TranslatePipe } from './pipes/translate.pipe';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TranslatePipe
+  ],
   imports: [
     CommonModule
   ],

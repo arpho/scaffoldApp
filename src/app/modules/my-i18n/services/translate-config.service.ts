7f23e16904165e061c8df106cde6520565169e0d
arpho <damicogiuseppe77@gmail.com>
2023-08-13 09:53:31 +0200

    translateModule in action
    used translate pipe

--- src/app/modules/my-i18n/services/translate-config.service.ts
+++ src/app/modules/my-i18n/services/translate-config.service.ts
@@ -4,14 +4,21 @@ import { TranslateService } from '@ngx-translate/core';
   providedIn: 'root'
 })
 export class TranslateConfigService {
+  setLanguage(lang: string) {
+    this._currentLanguage = lang
+    this.service.use(lang)
+  }
   _currentLanguage: string
 
-  set currentLanguage (lang){this._currentLanguage= lang}
+  set currentLanguage (lang){
+    this._currentLanguage= lang
+  this.service.use(lang)
+  }
   get currentLanguage (){
     return this._currentLanguage
   }
 
   constructor(private service:TranslateService) { 
-    this.currentLanguage = service.getDefaultLang()
+    this.currentLanguage = service.getDefaultLang()?service.getDefaultLang():'en' 
   }
 }
